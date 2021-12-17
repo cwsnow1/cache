7265963432effd258d29f074bdf14775f919374c
Caleb Snow <cwsnow@ncsu.edu>
2021-12-16 19:55:52 -0600

    Comments in list.h

--- inc/list.h
+++ inc/list.h
@@ -1,4 +1,3 @@
-
 #pragma once
 
 typedef struct double_list_element_s {
@@ -14,16 +13,54 @@ typedef struct double_list_s {
     uint64_t capacity;
 } double_list_t;
 
+/**
+ * @brief           Allocates a double list and sets capacity, no elements
+ * 
+ * @param capacity  The maximum capacity for this list
+ * @return          Pointer to the new list
+ */
 double_list_t *double_list__init_list(uint64_t capacity);
 
+/**
+ * @brief           Looks for a specific element in a list and removes it if extant
+ * 
+ * @param list      List to search in
+ * @param element   Element to search for
+ * @return true     if element was found & removed
+ */
 bool double_list__remove_element(double_list_t *list, double_list_element_t *element);
 
+/**
+ * @brief           Adds element to the end of a list if it has room
+ * 
+ * @param list      List to add element to
+ * @param element   Element to add
+ * @return true     if there was room & element was added
+ */
 bool double_list__add_element_to_tail(double_list_t *list, double_list_element_t *element);
 
+/**
+ * @brief           Adds element to head of a list if it has room
+ * 
+ * @param list      List to add element to
+ * @param element   Element to add
+ * @return true     if there was room & element was added
+ */
 bool double_list__push_element(double_list_t *list, double_list_element_t *element);
 
+/**
+ * @brief       Removes element from head of list and returns it
+ * 
+ * @param list  List to pop from
+ * @return      Pointer to element if there is one, NULL otherwise
+ */
 double_list_element_t *double_list__pop_element(double_list_t *list);
 
+/**
+ * @brief       Walk the list & free memory for all elements in list and frees this list
+ * 
+ * @param list  List to free
+ */
 void double_list__free_list(double_list_t *list);
 
 #define for_each_in_double_list(list)   double_list_element_t *element_i = list->head;                              \
