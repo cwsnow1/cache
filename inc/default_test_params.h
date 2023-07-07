1426e1816ca0f06d8926641547e429e38da07a75
Caleb Snow <snow.calebw@gmail.com>
2023-07-07 10:57:25 -0500

    reduce number of default configs

--- inc/default_test_params.h
+++ inc/default_test_params.h
@@ -1,12 +1,12 @@
 // Default cache test parameters that will be used
 // to generate the .ini file if it doesn't exist
 #define NUM_CACHE_LEVELS    (2)
-#define MIN_BLOCK_SIZE      (128)
+#define MIN_BLOCK_SIZE      (256)
 #define MAX_BLOCK_SIZE      (256)
 #define MIN_CACHE_SIZE      (MIN_BLOCK_SIZE * 16)
 #define MAX_CACHE_SIZE      (16384)
 #define MIN_ASSOCIATIVITY   (1)
-#define MAX_ASSOCIATIVITY   (4)
+#define MAX_ASSOCIATIVITY   (2)
 #define MAX_NUM_THREADS     (12) // -1 for no limit
 
 // Defined for simplification, not a test parameter
