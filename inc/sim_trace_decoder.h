11b7d54db9831e62e094c282962c53862693e0aa
Caleb Snow <snow.calebw@gmail.com>
2023-07-08 15:19:35 -0500

    printf windows fixes

--- inc/sim_trace_decoder.h
+++ inc/sim_trace_decoder.h
@@ -1,12 +1,13 @@
 #pragma once
+#include <inttypes.h>
 #include "SimTracer.h"
 
 constexpr sync_pattern_t kSyncPattern = 0xFFFFFFFF;
 
 // Update both these tables when adding a new entry
 const char* simTraceEntryDefinitions[kNumberOfSimTraceEntries] = {
-    "HIT:           pool_index=%02u, blockAddress=0x%04x%08lx, set_index=0x%08lx\n",
-    "MISS:          pool_index=%02u, requesting block in set_index=0x%08lx\n",
+    "HIT:           pool_index=%02u, blockAddress=0x%04x%08" PRIx64 ", set_index=0x%08" PRIx64 "\n",
+    "MISS:          pool_index=%02u, requesting block in set_index=0x%08" PRIx64 "\n",
     "LRU_UPDATE:    set_index=0x%08x, MRU: block_index=0x%02x, LRU: block_index=0x%02x\n",
     "EVICT:         set_index=0x%08x, block_index=0x%02x\n",
     "REQUEST_ADDED: pool_index=%02u, %c, address=0x%04x%08x, access_time=%u\n",
--- trace_generation/fake_trace_generator/trace.c
+++ trace_generation/fake_trace_generator/trace.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
 #define TRACE_LENGTH (0x100000)
 
 int main (int argc, char **argv) {
     FILE *f = fopen("seq.trace", "w");
     for (uint64_t i = 0; i < TRACE_LENGTH; ++i) {
-        fprintf(f, "0x000000000000: R 0x%012lx\n", i);
+        fprintf(f, "0x000000000000: R 0x%012" PRIx64 "\n", i);
     }
     return 0;
 }
